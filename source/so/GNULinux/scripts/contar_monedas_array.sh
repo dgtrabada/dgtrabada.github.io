6a63b6f608625731fdd2d080cb19c52d96294ad9
dgtrabada <dgtrabada@gmail.com>
2025-11-06 09:00:52 +0000

    script

--- source/so/GNULinux/scripts/contar_monedas_array.sh
+++ source/so/GNULinux/scripts/contar_monedas_array.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-for((i=0;i<10;i++)
+for((i=0;i<10;i++))
 do
   x[$i]=0
 done
 
 total=0
 
-for((i=0;i<$(wc -l monedas.dat | cut -d' ' -f);i++))
+for((i=0;i<$(wc -l monedas.dat | cut -d' ' -f1);i++))
 do
-  x=$(head -$i monedas.dat | tail -1 )
-  total=$((total+x))
+  monedas=$(head -$i monedas.dat | tail -1 )
+  total=$((total+monedas))
 
-  if [[ ${x[$i]} -eq $i ]]
-  then 
-    x[$i]=$((x[$i]+1))
-  fi
-
+  for((j=0;j<10;j++))
+  do
+    if [[ $monedas -eq $j ]]
+    then
+      x[$j]=$((x[$j]+1))
+    fi
+  done
 done
 
-for((i=0;i<10;i++)
+for((i=0;i<10;i++))
 do
   echo hay ${x[$i]} con $i euros
 done
